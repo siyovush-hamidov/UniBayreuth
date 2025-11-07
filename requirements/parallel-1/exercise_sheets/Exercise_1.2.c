643cbe64c015834e351b440c47a077a408f50909
siyovush-hamidov <siyovushchik14@gmail.com>
2025-11-07 10:46:34 +0100

    решил первый exercise sheet по параллельным

--- requirements/parallel-1/exercise_sheets/Exercise_1.2.c
+++ requirements/parallel-1/exercise_sheets/Exercise_1.2.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
+#include <time.h>
 
 typedef struct NODE Node;
 struct NODE
@@ -70,9 +72,168 @@ void tree_insert_key(Tree *t, int key)
                 else
                 {
                     current = current->larger_keys;
-                    return;
                 }
             }
         }
     }
+}
+
+int find_recursive(Node *current, int target_key)
+{
+    if (current == NULL)
+    {
+        return 0;
+    }
+    if (current->key == target_key)
+    {
+        return 1;
+    }
+    if (target_key < current->key)
+    {
+        return find_recursive(current->smaller_keys, target_key);
+    }
+    else if (target_key > current->key)
+    {
+        return find_recursive(current->larger_keys, target_key);
+    }
+}
+
+int tree_find_key_recursive(Tree *t, int target_key)
+{
+    return find_recursive(t->root, target_key);
+}
+
+int tree_find_key_iterative(Tree *t, int target_key)
+{
+    Node *current = t->root;
+    while (current != NULL)
+    {
+        if (current->key == target_key)
+        {
+            return 1;
+        }
+        if (target_key < current->key)
+        {
+            current = current->smaller_keys;
+        }
+        else if (target_key > current->key)
+        {
+            current = current->larger_keys;
+        }
+    }
+    return 0;
+}
+
+int is_valid_recursive(Node *current, int min_value, int max_value)
+{
+    if (current == NULL)
+    {
+        return 1;
+    }
+    if (current->key <= min_value || current->key >= max_value)
+    {
+        return 0;
+    }
+
+    int left, right;
+
+    left = is_valid_recursive(current->smaller_keys, min_value, current->key);
+    right = is_valid_recursive(current->larger_keys, current->key, max_value);
+
+    return left * right;
+}
+
+int tree_is_valid(Tree *t)
+{
+    Node *current = t->root;
+    return is_valid_recursive(current, INT_MIN, INT_MAX);
+}
+
+Node *copy_recursive(Node *current)
+{
+    if (current == NULL)
+    {
+        return NULL;
+    }
+    else
+    {
+        Node *new_node = create_node(current->key);
+
+        new_node->smaller_keys = copy_recursive(current->smaller_keys);
+        new_node->larger_keys = copy_recursive(current->larger_keys);
+
+        return new_node;
+    }
+}
+
+Tree tree_deep_copy(Tree *t)
+{
+    Tree new_tree;
+
+    new_tree.root = copy_recursive(t->root);
+
+    return new_tree;
+}
+
+void delete_recursive(Node *current)
+{
+    if (current == NULL)
+    {
+        return;
+    }
+    else
+    {
+        delete_recursive(current->smaller_keys);
+        delete_recursive(current->larger_keys);
+        free(current);
+    }
+}
+
+void tree_delete(Tree *t)
+{
+    delete_recursive(t->root);
+    t->root = NULL;
+}
+
+void print_in_order(Node *current)
+{
+    if (current == NULL)
+    {
+        return;
+    }
+    print_in_order(current->smaller_keys);
+    printf("%d ", current->key);
+    print_in_order(current->larger_keys);
+}
+
+int main()
+{
+    srand(time(NULL));
+
+    Tree t = tree_create();
+    int i = 0;
+    for (i; i < 15; i++)
+    {
+        tree_insert_key(&t, rand() % 30);
+    }
+
+    printf("Tree cotnents: ");
+    print_in_order(t.root);
+    printf("\n");
+
+    printf("Find 0 iterative: %i \n", tree_find_key_iterative(&t, 0));
+    printf("Find 50 iterative: %i \n", tree_find_key_iterative(&t, 50));
+
+    printf("Find 0 recursive: %i \n", tree_find_key_recursive(&t, 0));
+    printf("Find 50 recursive: %i \n", tree_find_key_recursive(&t, 50));
+
+    printf("Tree is valid: %i \n", tree_is_valid(&t));
+
+    Tree copy = tree_deep_copy(&t);
+
+    tree_delete(&t);
+    printf("Copy contents: ");
+    print_in_order(copy.root);
+    printf("\n");
+    tree_delete(&copy);
 }
